800381e5a0167f7a1fd828770e3222d417d03eb8
Josman Santiago Muñoz Mera <josmansant@unicauca.edu.co>
2023-12-02 21:41:06 -0500

    Vite

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://josmanm.github.io/TallerNivelacion'
+  base: 'https://josmanm.github.io/TallerNivelacion'
 })
